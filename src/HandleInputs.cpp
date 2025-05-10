2aca6e0d30e55474ef9df82e7a63fa44827c0d7c
Dale <dalemorris@mas-uk.com>
2025-05-10 19:07:54 +0100

    Auto‐update: 2025-05-10 19:07:54

--- src/HandleInputs.cpp
+++ src/HandleInputs.cpp
@@ -518,8 +518,8 @@ void updateBreakGlassInput() {
   // Current Timestamp
   uint32_t now = millis();
 
-  Serial.print("Checking Break Glass Input...");
-  Serial.println(systemData.bgDebouncer.read());
+  // Serial.print("Checking Break Glass Input...");
+  // Serial.println(systemData.bgDebouncer.read());
 
   // Check if the break-glass input has just been pressed.
   if (systemData.bgDebouncer.read() == LOW) 
