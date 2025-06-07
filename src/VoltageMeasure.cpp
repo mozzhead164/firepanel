bb50d0f1f3592288b87c6187842ab2981b7b6bd5
Dale <dalemorris@mas-uk.com>
2025-06-07 21:58:19 +0100

    Auto‐update: 2025-06-07 21:58:19

--- src/VoltageMeasure.cpp
+++ src/VoltageMeasure.cpp
@@ -11,13 +11,19 @@
 void read_psuVoltages() 
 {
 
+// Get Current Time Stamp
+uint32_t timeNow = millis();
+static uint32_t lastVoltage = 0UL;
+
+// Periodically Read PSU Voltages
+if(timeNow - lastVoltage >= 500UL)
+{
+
   #ifdef DEBUG_STARTUP
     Serial.println("\n Initializing PSU Voltage Measurement...\n");
   #endif
-  
-  // Get Current Time Stamp
-  uint32_t timeNow = millis();
 
+  
   static float psu1_voltage = 0.0;
   static float psu2_voltage = 0.0;
 
@@ -27,132 +33,127 @@ void read_psuVoltages()
   static uint8_t  psu1_restore_count  = 0;
   static uint8_t  psu2_restore_count  = 0;
 
-  static uint32_t lastVoltage = 0UL;
+
+  // Take Raw Reading - PSU 1
+  psu1_voltage = analogRead(ADC0);
+  psu1_voltage = (psu1_voltage / 1024.0) * 16.8;
+
+  // Take Raw Reading - PSU 2
+  psu2_voltage = analogRead(ADC1);
+  psu2_voltage = (psu2_voltage / 1024.0) * 16.8;
 
 
-  // Periodically Read PSU Voltages
-  if(timeNow - lastVoltage >= 250UL)
+
+
+
+  // Check For Under Voltage on PSU 1
+  if(psu1_voltage <= VOLTAGE_UNDER && !underVoltage_1) 
   {
-    // Take Raw Reading - PSU 1
-    psu1_voltage = analogRead(ADC0);
-    psu1_voltage = (psu1_voltage / 1024.0) * 16.8;
+    Serial.println("\n\n PSU 1 - UNDER VOLTAGE ALERT!!!");
+    underVoltage_1 = true;
 
-    // Take Raw Reading - PSU 2
-    psu2_voltage = analogRead(ADC1);
-    psu2_voltage = (psu2_voltage / 1024.0) * 16.8;
-
-
-
-
-
-    // Check For Under Voltage on PSU 1
-    if(psu1_voltage <= VOLTAGE_UNDER && !underVoltage_1) 
-    {
-      Serial.println("\n\n PSU 1 - UNDER VOLTAGE ALERT!!!");
-      underVoltage_1 = true;
-
-      systemData.psu1UnderVolt = true;    // update the flag
-      systemData.psu1Voltage   = psu1_voltage;  // add this member too
-      
-      // dispatch undervoltage event for PSU1
-      Event e{ EVENT_PSU_UNDERVOLTAGE, 1, false };
-      dispatchEvent(&e);
-
-      psu1_restore_count = 0;  // reset restore counter
-    }
+    systemData.psu1UnderVolt = true;    // update the flag
+    systemData.psu1Voltage   = psu1_voltage;  // add this member too
     
-    // Check For Voltage Restored on PSU 1
-    else if(underVoltage_1) 
-    { 
-      // re-read the voltage right now:
-      int raw = analogRead(ADC0);
-      float voltage = raw / 1024.0 * PSU1_VOLTAGE_SCALE; 
+    // dispatch undervoltage event for PSU1
+    Event e{ EVENT_PSU_UNDERVOLTAGE, 1, false };
+    dispatchEvent(&e);
 
-      if (voltage >= VOLTAGE_RESTORE) 
-      {
-          if (++psu1_restore_count >= 2) 
-          {
-            // OK, sustained high voltage
-            underVoltage_1     = false;
-            psu1_restore_count = 0;
+    psu1_restore_count = 0;  // reset restore counter
+  }
+  
+  // Check For Voltage Restored on PSU 1
+  else if(underVoltage_1) 
+  { 
+    // re-read the voltage right now:
+    int raw = analogRead(ADC0);
+    float voltage = raw / 1024.0 * PSU1_VOLTAGE_SCALE; 
 
-            Serial.print("\n PSU 1 - Voltage Restored @ ");
-            Serial.print(voltage, 2);
-            Serial.println(" V"); 
-
-            // stash voltage in systemData for handler to grab
-            raw = analogRead(ADC0);
-            voltage = raw / 1024.0 * PSU1_VOLTAGE_SCALE; 
-            systemData.psu1UnderVolt = false;    // update the flag
-            systemData.psu1Voltage   = voltage;  // add this member too
-
-            Event e{ EVENT_PSU_RESTORED, 1, false };
-            dispatchEvent(&e);
-          }
-      } else {
-        // voltage fell back below threshold, reset counter
-        psu1_restore_count = 0;
-      }
-    }
-    
-
-
-
-    // Check For Under Voltage on PSU 2
-    if(psu2_voltage <= VOLTAGE_UNDER && !underVoltage_2) 
+    if (voltage >= VOLTAGE_RESTORE) 
     {
-      Serial.println("\n\n PSU 2 - UNDER VOLTAGE ALERT!!!");
-      underVoltage_2 = true;
+        if (++psu1_restore_count >= 2) 
+        {
+          // OK, sustained high voltage
+          underVoltage_1     = false;
+          psu1_restore_count = 0;
 
-      systemData.psu2UnderVolt = true;    // update the flag
-      systemData.psu2Voltage   = psu2_voltage;  // add this member too
+          Serial.print("\n PSU 1 - Voltage Restored @ ");
+          Serial.print(voltage, 2);
+          Serial.println(" V"); 
 
-      // dispatch undervoltage event for PSU2
-      Event e{ EVENT_PSU_UNDERVOLTAGE, 2, false };
-      dispatchEvent(&e);
+          // stash voltage in systemData for handler to grab
+          raw = analogRead(ADC0);
+          voltage = raw / 1024.0 * PSU1_VOLTAGE_SCALE; 
+          systemData.psu1UnderVolt = false;    // update the flag
+          systemData.psu1Voltage   = voltage;  // add this member too
 
-      psu2_restore_count = 0;  // reset restore counter
+          Event e{ EVENT_PSU_RESTORED, 1, false };
+          dispatchEvent(&e);
+        }
+    } else {
+      // voltage fell back below threshold, reset counter
+      psu1_restore_count = 0;
     }
+  }
+  
 
-    // Check For Voltage Restored on PSU 2
-    else if(underVoltage_2) 
-    { 
-      // re-read the voltage right now:
-      int raw = analogRead(ADC1);
-      float voltage = raw / 1024.0 * PSU2_VOLTAGE_SCALE;
 
-      if (voltage >= VOLTAGE_RESTORE) 
-      {
-          if (++psu2_restore_count >= 2) 
-          {
-            // OK, sustained high voltage
-            underVoltage_2     = false;
-            psu2_restore_count = 0;
 
-            Serial.print("\n PSU 2 - Voltage Restored @ ");
-            Serial.print(voltage, 2);
-            Serial.println(" V"); 
+  // Check For Under Voltage on PSU 2
+  if(psu2_voltage <= VOLTAGE_UNDER && !underVoltage_2) 
+  {
+    Serial.println("\n\n PSU 2 - UNDER VOLTAGE ALERT!!!");
+    underVoltage_2 = true;
 
-            // stash voltage in systemData for handler to grab
-            raw = analogRead(ADC1);
-            voltage = raw / 1024.0 * PSU2_VOLTAGE_SCALE;
-            systemData.psu2UnderVolt = false;    // update the flag
-            systemData.psu2Voltage   = voltage; 
+    systemData.psu2UnderVolt = true;    // update the flag
+    systemData.psu2Voltage   = psu2_voltage;  // add this member too
 
-            Event e{ EVENT_PSU_RESTORED, 2, false };
-            dispatchEvent(&e);
-          }
-      } else {
-        // voltage fell back below threshold, reset counter
-        psu2_restore_count = 0;
-      }
+    // dispatch undervoltage event for PSU2
+    Event e{ EVENT_PSU_UNDERVOLTAGE, 2, false };
+    dispatchEvent(&e);
+
+    psu2_restore_count = 0;  // reset restore counter
+  }
+
+  // Check For Voltage Restored on PSU 2
+  else if(underVoltage_2) 
+  { 
+    // re-read the voltage right now:
+    int raw = analogRead(ADC1);
+    float voltage = raw / 1024.0 * PSU2_VOLTAGE_SCALE;
+
+    if (voltage >= VOLTAGE_RESTORE) 
+    {
+        if (++psu2_restore_count >= 2) 
+        {
+          // OK, sustained high voltage
+          underVoltage_2     = false;
+          psu2_restore_count = 0;
+
+          Serial.print("\n PSU 2 - Voltage Restored @ ");
+          Serial.print(voltage, 2);
+          Serial.println(" V"); 
+
+          // stash voltage in systemData for handler to grab
+          raw = analogRead(ADC1);
+          voltage = raw / 1024.0 * PSU2_VOLTAGE_SCALE;
+          systemData.psu2UnderVolt = false;    // update the flag
+          systemData.psu2Voltage   = voltage; 
+
+          Event e{ EVENT_PSU_RESTORED, 2, false };
+          dispatchEvent(&e);
+        }
+    } else {
+      // voltage fell back below threshold, reset counter
+      psu2_restore_count = 0;
     }
+  }
 
-    // Timestamp For Last Voltage Reading
-    lastVoltage = timeNow;
+  // Timestamp For Last Voltage Reading
+  lastVoltage = timeNow;
 
-    systemData.psu1Voltage = psu1_voltage;
-    systemData.psu2Voltage = psu2_voltage;
+  systemData.psu1Voltage = psu1_voltage;
+  systemData.psu2Voltage = psu2_voltage;
   }
 
 
